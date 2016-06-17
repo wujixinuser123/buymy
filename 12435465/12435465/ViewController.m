f7c4e188860f04505466a9fe01055fa18a8489b2
wujixinuser123 <wujixin8125@sina.com>
2016-06-17 15:14:20 +0800

    输出一句话

--- 12435465/12435465/ViewController.m
+++ 12435465/12435465/ViewController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    NSLog(@"sufhsehsrgldglkdl;hjkojs;lha容易；aehiiR");
 }
 
 - (void)didReceiveMemoryWarning {
